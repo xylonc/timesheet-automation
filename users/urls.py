628fe2f905546267988dde50d68010c0cc95ea93
xylon <101974919+xylonc@users.noreply.github.com>
2026-04-02 15:11:41 +0800

    Added login feature

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('registration/', views.register_view , name = 'register_view'),
+    path('registration/', views.register_view , name='register_view'),
+    path('login/', views.login_view , name='login_view'),
 ]
