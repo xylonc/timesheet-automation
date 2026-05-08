2137e6a5822f17c1f6d59fa4fb2e49cbccfa1e2d
xylon <101974919+xylonc@users.noreply.github.com>
2026-05-08 15:33:52 +0800

    fortified middleware.py

--- core/middleware.py
+++ core/middleware.py
@@ -1,26 +1,35 @@
-from django.shortcuts import redirect
+from django.urls import resolve , Resolver404
 from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
+
+EXEMPT_VIEW_NAMES = {
+    'users:login_view',
+    'users:password_reset',
+    'users:password_reset_done',
+    'users:password_reset_confirm',
+    'users:password_reset_complete',
+}
 
 class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
     
     def __call__(self,request):
-        #paths exempt from authentication
-        exempt_urls = [
-            settings.LOGIN_URL,
-            '/static/',
-            '/users/password_reset/',
-            '/users/reset/',
+        if request.user.is_authenticated:
+            return self.get_response(request)
+        
+        #static file bypass cannot use resolver 
+        if request.path.startswith(settings.STATIC_URL):
+            return self.get_response(request)
 
-        ]
-        if not request.user.is_authenticated:
-            if not any(request.path.startswith(url) for url in exempt_urls):
-                return redirect_to_login(
-                    request.get_full_path(),
-                    settings.LOGIN_URL,
-                    )
-        response = self.get_response(request)
-        return response
+        #paths exempt from authentication
+        
+        try:
+            match = resolve(request.path_info)
+            if match.view_name in EXEMPT_VIEW_NAMES:
+                return self.get_response(request)
+        except Resolver404:
+            pass
+
+        return redirect_to_login(request.get_full_path(), settings.LOGIN_URL)
         
