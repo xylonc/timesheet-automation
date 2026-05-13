5631c8e08731f4a1764d4930b29a56c39c9dc01f
xylon <101974919+xylonc@users.noreply.github.com>
2026-05-13 16:04:15 +0800

    Added the queryset authorisation for technician as a defence in depth layer

--- technicians/models.py
+++ technicians/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.conf import settings
 from core.permissions import Roles
 
+class TechnicianQuerySet(models.QuerySet):
+    def visible_to(self,user):
+        if user.is_superuser or user.groups.filter(name=Roles.ADMIN).exists():
+            return self
+        return self.filter(user=user)
+
+
 class Technician(models.Model):
+    objects = TechnicianQuerySet.as_manager()
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     technician_name = models.CharField(("technician name"), max_length=50)
     tech_phone = models.CharField(("Serviceman phone"), max_length=8)
